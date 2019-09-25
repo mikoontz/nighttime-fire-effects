3f695be8bd32a6fa10897947d61934620d603c22
Michael Koontz <mikoontz@gmail.com>
2019-09-25 15:47:43 -0600

    fix dateline issue when transforming the MODIS events

--- data/data_carpentry/prepare-FIRED-north-america-for-earth-engine.R
+++ data/data_carpentry/prepare-FIRED-north-america-for-earth-engine.R
@@ -3,9 +3,12 @@
 library(sf)
 library(tidyverse)
 
-fired <- sf::st_read("data/data_raw/FIRED_north-america/FIRED_north-america.shp")
+fired <- sf::st_read("data/data_raw/modis_events/modis_events.shp")
 
-fired_4326 <- st_transform(fired, 4326)
-
+fired_4326 <- 
+  fired %>% 
+  st_transform(crs = 4326) %>% 
+  st_wrap_dateline()
+  
 dir.create("data/data_output/FIRED_north-america")
 sf::st_write(fired_4326, "data/data_output/FIRED_north-america/FIRED_north-america.shp")
