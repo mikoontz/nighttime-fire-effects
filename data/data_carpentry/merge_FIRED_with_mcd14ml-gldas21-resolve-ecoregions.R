dcb214c47b093d0c282876993bd350dd31e72f70
Michael Koontz <mikoontz@gmail.com>
2019-10-03 10:19:38 -0600

    settle on the divide-and-conquer approach which grants the ability to run things in parallel more easily and which speeds up the code quite a bit (because there is much stricter subsetting prior to the spatial intersection)

--- data/data_carpentry/merge_FIRED_with_mcd14ml-gldas21-resolve-ecoregions.R
+++ data/data_carpentry/merge_FIRED_with_mcd14ml-gldas21-resolve-ecoregions.R
@@ -3,6 +3,7 @@
 
 # Try to build this such that it can run on AWS.
 
+# Load packages
 library(tidyverse)
 library(sf)
 library(data.table)
@@ -14,7 +15,7 @@ library(tictoc)
 library(doParallel)
 library(foreach)
 
-
+# Download the western hemisphere FIRED data from S3 if it doesn't exist on disk
 if(!file.exists(file.path("data", "data_raw", "FIRED", "western_hemisphere_to_may2019.gpkg"))) {
   dir.create(path = file.path("data", "data_raw", "FIRED"), showWarnings = FALSE, recursive = TRUE)
   fired_west <- save_object(object = "western_hemisphere_to_may2019.gpkg",
@@ -31,17 +32,53 @@ fired_west <-
                 start_day = day(start_date),
                 last_year = year(last_date),
                 last_month = month(last_date),
-                last_day = day(last_date))
+                last_day = day(last_date),
+                duration = as.numeric(last_date - start_date))
 
 fired_crs <- st_crs(fired_west)
 
+determine_burning_days <- 
+  function(start_date, last_date) {
+    burning_days_df <- 
+      format(seq(start_date,
+                 last_date,
+                 by = "day"), 
+             format = "%Y-%m-%d") %>% 
+      tibble() %>% 
+      separate(col = ".", into = c("year", "month", "day"), sep = "-") %>% 
+      dplyr::mutate(year = as.numeric(year),
+                    month = as.numeric(month),
+                    day = as.numeric(day))
+    
+    return(list(burning_days_df))
+  }
+
+v_determine_burning_days <- 
+  Vectorize(determine_burning_days, vectorize.args = c("start_date", "last_date"))
+
+fired <-
+  fired_west %>%
+  dplyr::filter(duration >= 0)
+
 mcd14ml_years <- 2000:2019
 mcd14ml_years_named <- mcd14ml_years %>% setNames(mcd14ml_years)
-i = 2000
 
 (start <- Sys.time())
+cl <- makeCluster(4)
+registerDoParallel(cl)
+
+subset_idx <- 1:2
+
 afd_list <- 
-  lapply(X = mcd14ml_years_named, FUN = function(i) {
+  foreach(i = mcd14ml_years_named[subset_idx], .packages = c("tidyverse", "data.table", "sf"), 
+          .final = function(x) setNames(x, names(mcd14ml_years_named)[subset_idx])) %dopar% {
+    
+    fired_by_day <-
+      fired %>%
+      dplyr::filter(start_year == i) %>% 
+      dplyr::mutate(burning_days = v_determine_burning_days(start_date, last_date)) %>% 
+      tidyr::unnest(cols = burning_days)
+    
     if(!file.exists(file.path("data", "data_output", "mcd14ml_gldas21_resolve-ecoregions", paste0("mcd14ml_gldas21_resolve-ecoregions_", i, ".csv")))) {
       download.file(url = "https://earthlab-mkoontz.s3-us-west-2.amazonaws.com/mcd14ml_gldas21_resolve-ecoregions/mcd14ml_gldas21_resolve-ecoregions_2000.csv",
                     destfile = file.path("data", "data_output", "mcd14ml_gldas21_resolve-ecoregions", paste0("mcd14ml_gldas21_resolve-ecoregions_", i, ".csv")))
@@ -55,274 +92,144 @@ afd_list <-
                     lat_sinu = sf::st_coordinates(.)[, 2]) %>% 
       data.table::as.data.table()
     
-    this_afd[, VERSION := as.numeric(VERSION)]
+    this_afd[, VERSION := as.character(VERSION)]
     
-    return(this_afd)
-  })
+    this_afd_by_monthday_list <- split(x = this_afd, by = c("acq_month", "acq_day"), sorted = TRUE)
+    x = names(this_afd_by_monthday_list)[273]
+    
+    spatiotemporal_match_fired_mcd14ml_list <-
+      lapply(names(this_afd_by_monthday_list), FUN = function(x) {
+        this_afd_name <- strsplit(x, split = "\\.")[[1]]
+        this_afd_year <- as.numeric(i)
+        this_afd_month <- as.numeric(this_afd_name[1])
+        this_afd_day <- as.numeric(this_afd_name[2])
+        
+        this_afd_monthday <- this_afd_by_monthday_list[[x]]
+        
+        this_afd_sf <- 
+          this_afd_monthday %>% 
+          sf::st_as_sf()
+        
+        this_spatiotemporal_match_fired_mcd14ml <-
+          fired_by_day %>% 
+          dplyr::filter(year == this_afd_year & month == this_afd_month & day == this_afd_day) %>% 
+          sf::st_intersection(this_afd_sf) %>% 
+          sf::st_drop_geometry() %>%
+          data.table::as.data.table()
+        
+        print(paste(this_afd_year, this_afd_month, this_afd_day, sep = "-"))
+        
+        return(this_spatiotemporal_match_fired_mcd14ml)
+        
+      })
+    
+   spatiotemporal_match_fired_mcd14ml <-
+      data.table::rbindlist(spatiotemporal_match_fired_mcd14ml_list)
+    
+    return(spatiotemporal_match_fired_mcd14ml)
+  }
+stopCluster(cl)
 
 # Add the TYPE column for the Near Real Time product
 afd_list[["2019"]]$TYPE <- NA_integer_
 afd_list[["2019"]] <- afd_list[["2019"]] %>% dplyr::select(names(afd_list[["2018"]]))
 
+afd <- data.table::rbindlist(afd_list)
 print(Sys.time() - start)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # second attempt to use vectorized operations on FIRED perimeters ---------
-spatiotemporal_match_to_fired = function(id, start_year, start_month, start_day, last_year, last_month, last_day, geom) {
-  
-  afd_idx <- which(names(afd_list) %in% seq(start_year, last_year))
-  
-  if(length(afd_idx) == 1) {
-    afd <- afd_list[[afd_idx]]
-  }
-  
-  if(length(afd_idx) > 1) {
-    afd <-
-      afd_list[afd_idx] %>% 
-      data.table::rbindlist(fill = TRUE)
-  }
-  
-  fired_afd_temporal_match <- afd[(acq_year >= start_year) & (acq_year <= last_year)
-                                  & (acq_month >= start_month) & (acq_month <= last_month)
-                                  & (acq_day >= start_day) & (acq_day <= last_day)]
-  
-  if(nrow(fired_afd_temporal_match) > 0) {
-    
-    fired_afd_spatiotemporal_match <-
-      fired_afd_temporal_match %>%
-      sf::st_as_sf() %>%
-      dplyr::filter(sf::st_intersects(., st_as_sf(st_geometry(geom), crs = fired_crs), sparse = FALSE)[, 1]) %>% 
-      sf::st_drop_geometry() %>%
-      data.table::as.data.table()
-    
-    if(nrow(fired_afd_spatiotemporal_match) == 0) {
-      fired_afd_spatiotemporal_match <- NULL
-    }
-    
-  } else {fired_afd_spatiotemporal_match <- NULL}
-  
-  rm(afd)
-  
-  return(fired_afd_spatiotemporal_match)
-}
+
+# spatiotemporal_match_to_fired = function(id, start_year, start_month, start_day, last_year, last_month, last_day, geom) {
+#   
+#   afd_idx <- which(names(afd_list) %in% seq(start_year, last_year))
+# 
+#   if(length(afd_idx) == 1) {
+#     afd <- afd_list[[afd_idx]]
+#   }
+# 
+#   if(length(afd_idx) > 1) {
+#     afd <-
+#       afd_list[afd_idx] %>%
+#       data.table::rbindlist(fill = TRUE)
+#   }
+#   
+#   fired_afd_temporal_match <- afd[(acq_year >= start_year) & (acq_year <= last_year)
+#                                   & (acq_month >= start_month) & (acq_month <= last_month)
+#                                   & (acq_day >= start_day) & (acq_day <= last_day)]
+#   
+#   if(nrow(fired_afd_temporal_match) > 0) {
+#     
+#     fired_afd_spatiotemporal_match <-
+#       fired_afd_temporal_match %>%
+#       sf::st_as_sf() %>%
+#       dplyr::filter(sf::st_intersects(., st_as_sf(st_geometry(geom), crs = fired_crs), sparse = FALSE)[, 1]) %>% 
+#       sf::st_drop_geometry() %>%
+#       data.table::as.data.table()
+#     
+#     if(nrow(fired_afd_spatiotemporal_match) == 0) {
+#       fired_afd_spatiotemporal_match <- NA
+#     }
+#     
+#   } else {fired_afd_spatiotemporal_match <- NA}
+#   
+#   rm(afd)
+#   
+#   return(list(fired_afd_spatiotemporal_match))
+# }
+
 
 # rowwise pmap solution ------------------------------------------------
-
-tic()
-
-afd_matches_to_fired <-
-  fired_west %>%
-  slice(1:1000) %>% 
-  dplyr::mutate(spatiotemporal_match = purrr::pmap(.l = list(id = id, 
-                                                             start_year = start_year,
-                                                             start_month = start_month,
-                                                             start_day = start_day,
-                                                             last_year = last_year,
-                                                             last_month = last_month,
-                                                             last_day = last_day,
-                                                             geom = geom), 
-                                                   .f = spatiotemporal_match_to_fired)) %>% 
-  dplyr::filter(!is.na(spatiotemporal_match)) %>%
-  tidyr::unnest(cols = spatiotemporal_match)
-
-toc()
-
-
-# rowwise foreach solution ------------------------------------------------
-
-fired <- fired_west[1:100, ]
-
-tic()
-
-cl <- makeCluster(4)
-registerDoParallel(cl)
-
-afd_matches_to_fired <- 
-  foreach(i = seq_along(1:nrow(fired)), 
-          .combine = rbind, 
-          .packages = c('tidyverse', 'sf', 'data.table')) %dopar% {
-            
-            fired %>% 
-              dplyr::slice(i) %>% 
-              dplyr::mutate(spatiotemporal_match = list(spatiotemporal_match_to_fired(id, start_year, start_month, start_day, last_year, last_month, last_day, geom))) %>% 
-              dplyr::filter(!is.na(spatiotemporal_match)) %>%
-              tidyr::unnest(cols = spatiotemporal_match) %>%
-              sf::st_drop_geometry()
-          }
-
-stopCluster(cl)
-
-afd_matches_to_fired <- as.data.table(afd_matches_to_fired)
-toc()
-
-
-
-
-?foreach
-
-
-
-
-
-# test_idx = 10
-# id = fired_west$id[test_idx]
-# start_year = fired_west$start_year[test_idx]
-# start_month = fired_west$start_month[test_idx]
-# start_day = fired_west$start_day[test_idx]
-# last_day = fired_west$last_day[test_idx]
-# last_month = fired_west$last_month[test_idx]
-# last_year = fired_west$last_year[test_idx]
-# geom = fired_west$geom[test_idx]
-
-# # Naive attempt at spatiotemporal join by iterating through each F --------
-# fired <- fired_west[1:100, ]
+# too slow methinks
 # 
-# (start <- Sys.time())
-# fired_afd_spatiotemporal_match_list <-
-#   purrr::pmap(.l = list(id = fired$id, 
-#                         start_year = fired$start_year,
-#                         start_month = fired$start_month,
-#                         start_day = fired$start_day,
-#                         last_year = fired$last_year,
-#                         last_month = fired$last_month,
-#                         last_day = fired$last_day,
-#                         geom = fired$geom), 
-#               .f = function(id, start_year, start_month, start_day, last_year, last_month, last_day, geom) {
-#                 
-#                 afd_idx <- which(names(afd_list) %in% seq(start_year, last_year))
-#                 
-#                 if(length(afd_idx) == 1) {
-#                   afd <- afd_list[[afd_idx]]
-#                 }
-#                 
-#                 if(length(afd_idx) > 1) {
-#                   afd <-
-#                     afd_list[afd_idx] %>% 
-#                     data.table::rbindlist(fill = TRUE)
-#                 }
-#                 
-#                 fired_afd_temporal_match <- afd[(acq_year >= start_year) & (acq_year <= last_year)
-#                                                 & (acq_month >= start_month) & (acq_month <= last_month)
-#                                                 & (acq_day >= start_day) & (acq_day <= last_day)]
-#                 
-#                 if(nrow(fired_afd_temporal_match) > 0) {
-#                   this_fired_sf <- st_as_sf(tibble(id, start_year, start_month, start_day, last_year, last_month, last_day, geom = st_geometry(geom)), crs = fired_crs)
-#                   
-#                   fired_afd_spatiotemporal_match <-
-#                     fired_afd_temporal_match %>%
-#                     sf::st_as_sf() %>%
-#                     sf::st_intersection(this_fired_sf) %>%
-#                     sf::st_drop_geometry() %>%
-#                     data.table::as.data.table()
-#                   
-#                   if(nrow(fired_afd_spatiotemporal_match) == 0) {
-#                     fired_afd_spatiotemporal_match <- NULL
-#                   }
-#                   
-#                 } else {fired_afd_spatiotemporal_match <- NULL}
-#                 
-#                 rm(afd)
-#                 
-#                 return(fired_afd_spatiotemporal_match)
-#               })
+# tic()
 # 
-# fired_matches <- 
-#   fired_afd_spatiotemporal_match_list %>% 
-#   purrr::compact() %>% 
-#   rbindlist()
+# afd_matches_to_fired <-
+#   fired %>%
+#   dplyr::filter(start_year == 2011) %>% 
+#   dplyr::mutate(spatiotemporal_match = purrr::pmap(.l = list(id = id, 
+#                                                              start_year = start_year,
+#                                                              start_month = start_month,
+#                                                              start_day = start_day,
+#                                                              last_year = last_year,
+#                                                              last_month = last_month,
+#                                                              last_day = last_day,
+#                                                              geom = geom), 
+#                                                    .f = spatiotemporal_match_to_fired)) %>% 
+#   dplyr::filter(!is.na(spatiotemporal_match)) %>%
+#   tidyr::unnest(cols = spatiotemporal_match)
 # 
-# (Sys.time() - start)
-# 
-# 
-# 
-# # third attempt that tries to use data.table -----------------------------
-# 
-# # Naive attempt at spatiotemporal join by iterating through each F --------
-# afd_small <- afd_list[[1]][1:100, ]
-# 
-# (start <- Sys.time())
-# 
-# spatiotemporal_match_to_fired <- function(acq_year, acq_month, acq_day, lon_sinu, lat_sinu) {
-#   
-#   fired_temporal_match <- 
-#     fired_west %>% 
-#     dplyr::filter(start_year <= acq_year & last_year >= acq_year & start_month <= acq_month & last_month >= acq_month & start_day <= acq_day & last_day >= acq_day) 
-#   # %>% 
-#   #   sf::st_intersection(st_as_sf(Vectorize(st_point(c(lon_sinu, lat_sinu))), crs = fired_crs))
-#   
-#   return(list(fired_temporal_match))
-# }
-# 
-# DT <- as.data.table(afd_small)
-# 
-# test <-
-#   DT[, spatiotemporal_match := list(fired_west[(fired_west$start_year <= acq_year) & (fired_west$last_year >= acq_year) 
-#                                                & (fired_west$start_month <= acq_month) & (fired_west$last_month >= acq_month) 
-#                                                & (fired_west$start_day <= acq_day) & (fired_west$last_day >= acq_day), ])]
-# %>% 
-#   dplyr::mutate(spatiotemporal_match = list(fired_west[(fired_west$start_year <= acq_year) & (fired_west$last_year >= acq_year) 
-#                                                        & (fired_west$start_month <= acq_month) & (fired_west$last_month >= acq_month) 
-#                                                        & (fired_west$start_day <= acq_day) & (fired_west$last_day >= acq_day), ]))
-# test %>% nest(cols = c(spatiotemporal_match = spatiotemporal_match))
-# 
-# test <-
-#   afd_small %>% 
-#   dplyr::mutate(spatiotemporal_match = list(fired_west[(fired_west$start_year <= acq_year) & (fired_west$last_year >= acq_year) 
-#                                                        & (fired_west$start_month <= acq_month) & (fired_west$last_month >= acq_month) 
-#                                                        & (fired_west$start_day <= acq_day) & (fired_west$last_day >= acq_day), ]))
-# 
-# 
-# # fired_afd_spatiotemporal_match_list <-
-# #   purrr::pmap(.l = list(id = fired$id, 
-# #                         start_year = fired$start_year,
-# #                         start_month = fired$start_month,
-# #                         start_day = fired$start_day,
-# #                         last_year = fired$last_year,
-# #                         last_month = fired$last_month,
-# #                         last_day = fired$last_day,
-# #                         geom = fired$geom), 
-# #               .f = function(id, start_year, start_month, start_day, last_year, last_month, last_day, geom) {
-# #                 
-# #                 afd_idx <- which(names(afd_list) %in% seq(start_year, last_year))
-# #                 
-# #                 if(length(afd_idx) == 1) {
-# #                   afd <- afd_list[[afd_idx]]
-# #                 }
-# #                 
-# #                 if(length(afd_idx) > 1) {
-# #                   afd <-
-# #                     afd_list[afd_idx] %>% 
-# #                     data.table::rbindlist(fill = TRUE)
-# #                 }
-# #                 
-# #                 fired_afd_temporal_match <- afd[(acq_year >= start_year) & (acq_year <= last_year)
-# #                                                 & (acq_month >= start_month) & (acq_month <= last_month)
-# #                                                 & (acq_day >= start_day) & (acq_day <= last_day)]
-# #                 
-# #                 if(nrow(fired_afd_temporal_match) > 0) {
-# #                   this_fired_sf <- st_as_sf(tibble(id, start_year, start_month, start_day, last_year, last_month, last_day, geom = st_geometry(geom)), crs = fired_crs)
-# #                   
-# #                   fired_afd_spatiotemporal_match <-
-# #                     fired_afd_temporal_match %>%
-# #                     sf::st_as_sf() %>%
-# #                     sf::st_intersection(this_fired_sf) %>%
-# #                     sf::st_drop_geometry() %>%
-# #                     data.table::as.data.table()
-# #                   
-# #                   if(nrow(fired_afd_spatiotemporal_match) == 0) {
-# #                     fired_afd_spatiotemporal_match <- NULL
-# #                   }
-# #                   
-# #                 } else {fired_afd_spatiotemporal_match <- NULL}
-# #                 
-# #                 rm(afd)
-# #                 
-# #                 return(fired_afd_spatiotemporal_match)
-# #               })
-# # 
-# # fired_matches <- 
-# #   fired_afd_spatiotemporal_match_list %>% 
-# #   purrr::compact() %>% 
-# #   rbindlist()
-# 
-# (Sys.time() - start)
+# toc()
+
 
