a9116da29b31d092c436d82a590c13eecedd3af5
Michael Koontz <mikoontz@gmail.com>
2020-06-16 13:27:21 -0600

    finalize sun angle calculations for re-classifying MCD14ML data

--- data/data_carpentry/solar-elevation-angle-calculation.R
+++ data/data_carpentry/solar-elevation-angle-calculation.R
@@ -6,6 +6,10 @@
 library(tidyverse)
 library(data.table)
 library(lubridate)
+library(oce) # Not run, but checked against our roll-your-own method with good agreement
+# The oce::sunAngle() method produces slightly more extreme sun angles than our method
+# (i.e., sun higher than we predict during peak daytime and sun lower than we predict during
+# peak nighttime)
 
 get_mcd14ml <- function(year, download = FALSE) {
   
@@ -42,34 +46,41 @@ add_solar_elev_angle <- function(this_afd) {
   this_afd[, acq_hour := floor(ACQ_TIME / 100)]
   this_afd[, acq_min := ((ACQ_TIME / 100) - acq_hour) * 100]
   this_afd[, acq_datetime := as.POSIXct((ACQ_DATE / 1000) + (acq_hour * 3600) + (acq_min * 60), 
-                                   origin = "1970-01-01", 
-                                   tz = "America/Los_Angeles")]
+                                        origin = "1970-01-01", 
+                                        tz = "America/Los_Angeles")]
+  
+  this_afd[, acq_datetime := lubridate::force_tz(acq_datetime, tzone = "UTC")]
   
   this_afd[, `:=`(acq_year = year(acq_datetime),
-             acq_month = month(acq_datetime),
-             acq_day = day(acq_datetime),
-             solar_offset = LONGITUDE / 15,
-             hemisphere = ifelse(LATITUDE >= 0, yes = "Northern hemisphere", no = "Southern hemisphere"))]
+                  acq_month = month(acq_datetime),
+                  acq_day = day(acq_datetime),
+                  solar_offset = LONGITUDE / 15,
+                  hemisphere = ifelse(LATITUDE >= 0, yes = "Northern hemisphere", no = "Southern hemisphere"))]
   
   this_afd[, acq_datetime_local := acq_datetime + as.duration(solar_offset * 60 * 60)]
   
   this_afd[, `:=`(local_doy = lubridate::yday(acq_datetime_local),
-             local_hour_decmin = ((acq_hour) + (acq_min / 60) + solar_offset + 24) %% 24)]
+                  local_hour_decmin = ((acq_hour) + (acq_min / 60) + solar_offset + 24) %% 24)]
   
   this_afd[, `:=`(local_solar_hour_decmin_round = round(local_hour_decmin),
-             local_solar_hour_decmin_round0.5 = round(local_hour_decmin * 2) / 2)]
+                  local_solar_hour_decmin_round0.5 = round(local_hour_decmin * 2) / 2)]
   
   # https://en.wikipedia.org/wiki/Solar_zenith_angle
   # https://en.wikipedia.org/wiki/Position_of_the_Sun#Declination_of_the_Sun_as_seen_from_Earth
   # https://en.wikipedia.org/wiki/Hour_angle
   
   this_afd[, `:=`(h = (local_hour_decmin - 12) * 15 * pi / 180,
-             phi = LATITUDE * pi / 180,
-             delta = -asin(0.39779 * cos(pi / 180 * (0.98565 * (local_doy + 10) + 360 / pi * 0.0167 * sin(pi / 180 * (0.98565 * (local_doy - 2)))))))]
+                  phi = LATITUDE * pi / 180,
+                  delta = -asin(0.39779 * cos(pi / 180 * (0.98565 * (local_doy + 10) + 360 / pi * 0.0167 * sin(pi / 180 * (0.98565 * (local_doy - 2)))))))]
   
   this_afd[, solar_elev_ang := (asin(sin(phi)*sin(delta) + cos(phi)*cos(delta)*cos(h))) * 180 / pi]
   
-  this_year <- as.numeric(this_afd[1, "acq_year"])
+  # afd[, solar_elev_ang2 := oce::sunAngle(t = acq_datetime,
+  #                                        longitude = LONGITUDE, 
+  #                                        latitude = LATITUDE, 
+  #                                        useRefraction = FALSE)$altitude]
+
+    this_year <- as.numeric(this_afd[1, "acq_year"])
   
   print(this_year)
   
@@ -77,15 +88,15 @@ add_solar_elev_angle <- function(this_afd) {
 }
 
 write_afd_solar_elev_angle <- function(this_afd) {
-
-    this_year <- as.numeric(this_afd[1, "acq_year"])
-
-    fwrite(this_afd, paste0("data/data_output/mcd14ml_solar-elevation-angle/mcd14ml_solar-elevation-angle_", this_year, ".csv"))
-    
-    print(this_year)
+  
+  this_year <- as.numeric(this_afd[1, "acq_year"])
+  
+  fwrite(this_afd, paste0("data/data_output/mcd14ml_solar-elevation-angle/mcd14ml_solar-elevation-angle_", this_year, ".csv"))
+  
+  print(this_year)
 }
 
-afd <- lapply(2000:2019, get_mcd14ml)
+afd <- lapply(2001:2019, get_mcd14ml)
 
 lapply(afd, add_solar_elev_angle)
 
